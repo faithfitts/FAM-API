d3cb03b28595dbfab15d15b7844f5a56f4567afa
faithfitts <faithfitts@gmail.com>
2021-02-18 11:40:24 -0800

    Add Models and Routes
    - Added Models for Posts and Comments
    - Added Routes for Posts and Comments
    - Updated User Schema to include Username
    - Updated Post Schema to include imageURL
    - Deleted Example Files

---
+++ app/models/post.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const commentSchema = require('./comment.js')
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  imageURL: {
+    type: String,
+    required: false
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  liked: {
+    type: Boolean,
+    default: false
+  },
+  comments: [commentSchema],
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+const Post = mongoose.model('Post', postSchema)
+
+module.exports = Post
